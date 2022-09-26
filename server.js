0c266ed5436473b5d17e5dfcf6440be0d744629b
Mark Andrey <markandreydelacruz@ymail.com>
2022-09-26 17:30:00 +0800

    added users get and post

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const users = [];
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+app.post('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const salt = yield bcrypt_1.default.genSalt();
+        const hashedPassword = yield bcrypt_1.default.hash(req.body.password, salt);
+        const user = {
+            name: req.body.name,
+            password: hashedPassword,
+        };
+        users.push(user);
+        res.status(201).send();
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send();
+    }
+}));
 app.listen(3000);
--- server.ts
+++ server.ts
@@ -1,5 +1,38 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 
 const app = express();
+app.use(express.json());
+
+interface User {
+  name: string;
+  password: string;
+}
+
+const users: User[] = [];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user: User = {
+      name: req.body.name,
+      password: hashedPassword,
+    };
+
+    users.push(user);
+
+    res.status(201).send();
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send();
+  }
+});
 
 app.listen(3000);
